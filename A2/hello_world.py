ec7845eb3ed517a912fea9c8e3198e47bf8579a2
Zack Cochran <59925779+ZackC55@users.noreply.github.com>
2020-01-16 14:41:26 -0500

    Create hello_world.py

---
+++ A2/hello_world.py
@@ -0,0 +1,18 @@
+"""
+Author: Zack Cochran
+Date: 01/16/2020
+Goal: To print a Hello world statement.
+"""
+
+# Declare variables
+my_name = 'Zack Cochran'
+age = 21
+major = 'Computer Information Systems & Accounting Double Major'
+place = 'Gallup, NM'
+
+# Hello world and three statements about myself
+print('Hello world,')
+print(' - my name is ' + my_name + '.')
+print(' - I am ' + str(age) + ' years old.')
+print(' - I am a ' + major + '.')
+print(' - I came to Cleveland, TN from ' + place + '.')
